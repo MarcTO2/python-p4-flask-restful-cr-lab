bd1151c3c18b3dceea68ba0e4efbaf26712d3453
Absalom <mrclabsalom@gmail.com>
2023-10-08 19:08:57 +0300

    Completed

--- server/models.py
+++ server/models.py
@@ -5,5 +5,8 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
+    name = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Decimal(10), nullable=False)
 
     id = db.Column(db.Integer, primary_key=True)
